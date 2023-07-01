baba6a2e9106b2d7024f220974645ca8fae2531c
Austin Mazur <mazuraustin1@gmail.com>
2023-07-01 11:46:38 +0200

    Update login, signup and context files

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,7 +1,7 @@
 import "./SignupPage.css";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import authService from "../../services/auth.service";
+import axios from "axios";
 
 function SignupPage() {
   const [email, setEmail] = useState("");
@@ -21,28 +21,20 @@ function SignupPage() {
     const requestBody = { email, password, name };
 
     // Send a request to the server using axios
-    /* 
+    
     const authToken = localStorage.getItem("authToken");
     axios.post(
       `${process.env.REACT_APP_SERVER_URL}/auth/signup`, 
       requestBody, 
       { headers: { Authorization: `Bearer ${authToken}` },
     })
-    .then((response) => {})
-    */
-
-    // Or using a service
-    authService
-      .signup(requestBody)
-      .then((response) => {
-        // If the POST request is successful redirect to the login page
-        navigate("/login");
-      })
-      .catch((error) => {
-        // If the request resolves with an error, set the error message in the state
-        const errorDescription = error.response.data.message;
-        setErrorMessage(errorDescription);
-      });
+    .then((response) => {
+      navigate("/login")
+    })
+    .catch((error) => {
+      const errorDescription = error.response.data.message;
+      setErrorMessage(errorDescription);
+    })
   };
 
   return (
