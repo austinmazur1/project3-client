3778bd2a6b856b57649ab02e28526a2a3157d3eb
Austin Mazur <mazuraustin1@gmail.com>
2023-07-13 21:01:22 +0200

    Add logic to auth for user specific route handling

---
+++ src/components/Role/IsSeller.jsx
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import { AuthContext } from "../../context/auth.context";
+import { Navigate } from "react-router-dom";
+// import Loading from "../Loading/Loading";
+
+function IsSeller({ children }) {
+  const { isSeller, isBuyer, isLoggedIn } = useContext(AuthContext);
+
+  // If the authentication is still loading ⏳
+  if (isSeller) {
+    return children
+  } else if(isBuyer) {
+    return <Navigate to="/buyer" />
+  }
+}
+
+export default IsSeller;
