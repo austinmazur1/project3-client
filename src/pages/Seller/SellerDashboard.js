f727bf77ebb4f7ba21d06d20ac1fd5893579ccfe
Asier González <asiergontz8@gmail.com>
2023-07-18 19:01:20 +0200

    getting userid

--- src/pages/Seller/SellerDashboard.js
+++ src/pages/Seller/SellerDashboard.js
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
- 
+import ProductFetchSeller from "../../components/containers/ProductFetchSeller";
  
  
 function ProductListPage() {
-  const [products, setProducts] = useState([]);
+ /* const [products, setProducts] = useState([]);
  
   const getAllProducts = () => {
     axios
@@ -16,19 +16,27 @@ function ProductListPage() {
  
   useEffect(() => {
     getAllProducts();
-  }, [] );
+  }, [] );*/
+
+    const renderProduct = (product) => {
+      return (
+        <div key={product._id}>
+                <h1>{product.productName}</h1>
+                <p>{product.description}</p>
+                <Link to={`/seller/${product._id}`}>
+                    <button>View product</button>
+                </Link>
+            </div>
+      )
+    }
  
   
   return (
     <div>
       <h1>Seller dashboard</h1>
-        {products.map((product) => {
-          return (
-            <div  key={product._id} >
-              <h3>{product.name}</h3>
-            </div>
-          );
-        })}     
+
+      <ProductFetchSeller  url={`${process.env.REACT_APP_SERVER_URL}/seller/dashboard`} render={renderProduct} />
+
         <Link to="/seller/new-product">
             <button>Add a product</button>
           </Link>
