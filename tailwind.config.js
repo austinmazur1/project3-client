9f9f8d7e252b131cb6f1de5a03ba291debd7967f
Asier González <asiergontz8@gmail.com>
2023-07-24 20:24:17 +0200

    signup page doing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.js', './src/**/*.jsx'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
 
